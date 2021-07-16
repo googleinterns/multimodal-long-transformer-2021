01173523d75d928ceef16aa5ba0be07b0a3fea81
roylu <roylu@uw.edu>
2021-07-16 17:40:55 +0000

    count text len and convert image to a sequence of patches (token embeddings)

--- src/input_utils.py
+++ src/input_utils.py
@@ -4,25 +4,41 @@ import tensorflow_text as tf_text
 
 from typing import List
 
+from google_research.etcmodel.models.modeling import EtcConfig 
+
 
 @attr.s
 class PretrainInputConfig(object):
-  
   """Config options for pretraining model input."""
 
   image_size = attr.ib(default=224)
   text_keys = attr.ib(factory=List)
+  patch_size = attr.ib(default=16)
+  patch_order = attr.ib(default='snake')
+  mlm_use_whole_word = attr.ib()  # type: bool
 
 
 def get_pretrain_example_decode_fn(tokenizer: tf_text.BertTokenizer,
                                    input_config: PretrainInputConfig,
+                                   model_config: EtcConfig,
                                    is_training: bool):
   """Returns a decode function to parse a single example into Tensors."""
 
   image_size = input_config.image_size
+  patch_size = input_config.patch_size
+  num_patch_per_row = image_size // patch_size
+
   name_to_features = {'image_data': tf.io.FixedLenFeature([], tf.string)}
   for k in input_config.text_keys:
     name_to_features[k] = tf.io.FixedLenFeature([], tf.string, default_value='')
+
+
+  def patch_reorder(im, mode='snake'):
+    if mode == 'snake':
+      return tf.reshape(im, [num_patch_per_row**2, (patch_size**2)*3])
+    else:
+      raise ValueError(f'Reordering mode ({mode}) is not available.')
+
 
   def _decode_fn(record):
     example = tf.io.parse_single_example(record, name_to_features)
@@ -45,11 +61,26 @@ def get_pretrain_example_decode_fn(tokenizer: tf_text.BertTokenizer,
         im = tf.image.flip_left_right(im)
     else:
       im = tf.image.resize(im, [image_size, image_size])
+    
+    # Create a sequence of patches (token)
+    im = tf.expand_dims(im, axis=0)
+    im = tf.image.extract_patches(im,
+                                  sizes=[1, patch_size, patch_size, 1],
+                                  strides=[1, patch_size, patch_size, 1],
+                                  rates=[1, 1, 1, 1],
+                                  padding = "VALID")
+    im = tf.squeeze(im, axis=0)
+    im = patch_reorder(im, mode=input_config.patch_order)
     example['image_data'] = im
 
     # Text
     for k in input_config.text_keys:
-      example[k] = tokenizer.tokenize(txt)
+      ids = tokenizer.tokenize(example[k]).merge_dims(-2, -1)
+      example[k] = ids 
+      example[f'len_{k}'] = tf.size(ids)
+
+    # (roylu) TODO
+    # Get MLM features
 
     return example
 
